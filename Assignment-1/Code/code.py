31eba553dbb0acf9521d38c9e523ca4e5e3c91dc
Anshul-Sangrame <102896632+Anshul-Sangrame@users.noreply.github.com>
2022-04-05 17:51:05 +0530

    Add files via upload

---
+++ Assignment-1/Code/code.py
@@ -0,0 +1,29 @@
+"""
+Name: Anshul Sangrame
+Roll Number: CS21BTECH11004
+
+ICSE class 10 2018 question 1(a)
+"""
+
+
+import numpy as np
+
+
+
+
+a = np.array([[6,-7],[4,5]])
+b = np.array([[10,7],[22,15]])
+
+c = (b-a)/2
+
+x = c[0,0]
+y = c[1,1] + 7
+
+print("x is",x,"and y is",y)
+
+
+
+
+
+
+
