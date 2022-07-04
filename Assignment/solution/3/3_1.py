18e8eff3f716b0be098a026ebc0177c33c42c00a
Anshul-Sangrame <anshul.sangrame@gmail.com>
2022-07-04 23:24:01 +0530

    3

--- Assignment/solution/3/3_1.py
+++ Assignment/solution/3/3_1.py
@@ -12,13 +12,13 @@ import matplotlib.pyplot as plt
 
 
 
-x = np.linspace(-4,4,30)#points on the x axis
+x = np.linspace(-4,4,1000)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('V.dat',dtype='double')
 #randvar = np.loadtxt('gau.dat',dtype='double')
-for i in range(0,30):
+for i in range(0,1000):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
