8a37f0b2c602774ca459abf5e998db56566b56dd
Anshul-Sangrame <anshul.sangrame@gmail.com>
2022-07-05 10:07:51 +0530

    update

--- Assignment/solution/1/1_2.py
+++ Assignment/solution/1/1_2.py
@@ -2,6 +2,7 @@
 #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 #if using termux
 
@@ -12,19 +13,29 @@ import matplotlib.pyplot as plt
 
 
 
-x = np.linspace(-4,4,30)#points on the x axis
+x = np.linspace(-4,4,100)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('uni.dat',dtype='double')
 #randvar = np.loadtxt('gau.dat',dtype='double')
-for i in range(0,30):
+for i in range(0,100):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
-	
-plt.plot(x.T,err)#plotting the CDF
+def cdf(x):
+	if x>=0 and x<=1:
+		return x
+	elif x>1:
+		return 1
+	else:
+		return 0
+
+vect = scipy.vectorize(cdf,otypes=[np.float64])	
+
+plt.plot(x.T,err,"o")#plotting the CDF
+plt.plot(x,vect(x))
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
